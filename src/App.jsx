bbd5824e02ddafce6c81ea910bd44e19caee6eda
adriamdevs <murigucharles3@gmail.com>
2025-11-25 12:11:53 +0300

    resolve all pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,15 @@ import Header from './components/header'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
   return (
     <nav className='bg-black/80'>
       <Header/>
       <div className="h-16 w-full items-center text-3xl text-white flex justify-between">
         <h1>Murigu Gitonga</h1>
       </div>
-      <div>
-        
+      <div> 
       </div>
     </nav>
     
