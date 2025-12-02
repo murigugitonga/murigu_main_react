c13a4fe4f3112ee8f671dea5430ff3c5ed2332fd
adriamdevs <murigucharles3@gmail.com>
2025-12-02 09:38:13 +0300

    modify workflow & add PAT token

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return(
-        <div className="bg=[#faf9f6] border-x border-t px-10 border-gray-600 rounded-t-lg p-4 flex">
-            <div className="flex flex-col space-y-1 items-center text-gray-600 text-xs">
+        <div className="bg=[#faf9f6] border-x border-t px-10 border-blue-600 rounded-t-lg p-4 flex">
+            <div className="flex flex-col space-y-0.5 items-center text-gray-600 text-xs">
                 <h3>Murigu Gitonga &copy; { new Date().getFullYear()}</h3>
                 <p>Powered by React & Tailwind</p>
                 <p>All Rights Reserved.</p>
