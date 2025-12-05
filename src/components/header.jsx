833d728d7fb109e3e7fa5a43001a00816de84d08
adriamdevs <murigucharles3@gmail.com>
2025-12-05 13:43:34 +0300

    run local build

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,8 +2,8 @@ import { logoimage } from "../assets/images"
 import  MessageMenu  from "./MessageMenu"
 export default function Header() {
     return (
-        <div className="flex items-center justify-between border-b border-gray-700/40 w-full shadow backdrop-blur-sm md:bg-inherit fixed top-0 z-30 py-2.5 bg-inherit">
-            <div className="md:mx-4 lg:container lg:mx-auto">
+        <div className="flex px-4 items-center justify-between border-b border-gray-700/40 w-full shadow backdrop-blur-sm md:bg-inherit fixed top-0 z-30 py-2.5 bg-inherit">
+            <div className="lg:container lg:mx-auto">
                 <img src={logoimage} alt="Logo" className="w-12 h-auto md:w-16"/>
             </div>
             {/**Message icon & darkmode icon */}
