3f04d31b2917423903811362b4efb765dc846680
adriamdevs <murigucharles3@gmail.com>
2025-12-07 22:17:03 +0300

    correct url typo

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -12,7 +12,7 @@ export default function PersonalDetail() {
             <div className="flex flex-col items-center w-full space-y-4">
                 <div className="flex flex-col items-center space-y-0">
                     <h2 className="text-xl font-medium">Murigu Gitonga</h2>
-                    <h3 className="text-[16px] font-normal">Computer scientist</h3>
+                    <h3 className="text-[16px] font-normal">Software Engineer</h3>
                 </div>
                 <div className="grid w-full grid-cols-2 gap-5 text-sm md:grid-cols-1 xl:grid-cols-2 place-items-center">
                     {/* <SkillMarquee/> */}
@@ -21,8 +21,9 @@ export default function PersonalDetail() {
                     <span className="bg-gray-900/85 py-0.5 px-2 text-gray-300 md:text-center whitespace-nowrap">Systems Engineering</span> 
                     <span className="bg-gray-900/85 py-0.5 px-2 justify-self-center col-span-2 text-gray-300 whitespace-nowrap md:text-center">Software Development</span>
                 </div>
-                <div className="flex justify-center px-4 italic text-gray-500 md:px-0">
+                <div className="flex flex-col space-y-3 justify-center px-4 italic text-gray-500 md:px-0">
                     <p className="text-sm text-center">Hi! I specialize in building integrated systems that combine artificial intelligence, modern engineering principles and robust software architecture.</p>
+                    <p className="text-sm text-center">Welcome to my website</p>
                 </div>
             </div>
         </div>
