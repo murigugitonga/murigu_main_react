c0d8de6f3b60857c4309094e85a7a0cef7eeb0d6
adriamdevs <murigucharles3@gmail.com>
2025-12-12 21:15:46 +0300

    edit linkedIn

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ export default function Footer(){
             <a href="https://www.facebook.com/profile.php?id=100073308548686" target="_blank">
                 <FaFacebook className="w-6 h-6 text-gray-700 transition hover:text-blue-600"/>
             </a>
-            <a href="www.linkedin.com/in/murigugitonga" target="_blank">
+            <a href="https://www.linkedin.com/in/murigugitonga?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank">
                 <FaLinkedin className="w-6 h-6 text-gray-700 transition hover:text-blue-600"/>
-            </a>
+            </a>00
             <a href="https://x.com/Imurigugitonga?t=i-3-KYfZ_dKrTgHLqH3jig&s=08" target="_blank">
                 <FaXTwitter className="w-6 h-6 text-gray-700 transition hover:text-blue-600"/>
             </a>
@@ -18,7 +18,7 @@ export default function Footer(){
             <div className="flex flex-col space-y-0.5 items-center text-gray-600 text-[10px]">
                 <h3>Murigu Gitonga &copy; { new Date().getFullYear()}.</h3>
                 <p>All Rights Reserved.</p>
-                <p>Powered by reactjs & tailwindcss.</p>
+                <p>Powered by react & tailwind.</p>
             </div>      
          </div>
         </div>
