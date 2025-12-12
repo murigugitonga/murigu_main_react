5f340462d10422b061d629d322ae95293c7fe79b
adriamdevs <murigucharles3@gmail.com>
2025-12-12 21:50:05 +0300

    add medium link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
-import { FaFacebook, FaXTwitter, FaLinkedin } from "react-icons/fa6"
+import {  } from "react-icons/fa"
+import { FaFacebook, FaXTwitter, FaLinkedin, FaMedium, FaMediumM } from "react-icons/fa6"
 export default function Footer(){
     return(
         <>
@@ -9,9 +10,12 @@ export default function Footer(){
             </a>
             <a href="https://www.linkedin.com/in/murigugitonga?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank">
                 <FaLinkedin className="w-6 h-6 text-gray-700 transition hover:text-blue-600"/>
-            </a>00
+            </a>
             <a href="https://x.com/Imurigugitonga?t=i-3-KYfZ_dKrTgHLqH3jig&s=08" target="_blank">
                 <FaXTwitter className="w-6 h-6 text-gray-700 transition hover:text-blue-600"/>
+            </a>
+            <a href="https://medium.com/@Murigucharles" target="_blank">
+                <FaMediumM className="w-6 h-6 text-gray-700 transition hover:text-blue-600"/>
             </a>
          </nav>
          <div className="bg=[#faf9f6] items-center border-x border-t px-10 mx-12 md:max-w-96 border-gray-700/40 rounded-t-lg p-4 flex">
