09cb8e23a0809ebba1fe55c855d60bda32d36654
adriamdevs <murigucharles3@gmail.com>
2025-12-12 20:46:23 +0300

    add external sites hyperlinks

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ import  MessageMenu  from "./MessageMenu"
 
 export default function Header() {
     return (
-        <header className="flex px-4 lg:container lg:mx-auto rounded-lg shadow-sm items-center justify-between border-b border-gray-700/40 w-full backdrop-blur-sm md:bg-inherit fixed top-0 z-30 py-2.5 bg-inherit">
+        <header className="flex px-4 lg:container lg:mx-auto rounded-b-lg shadow-sm items-center justify-between border-b border-gray-700/40 w-full backdrop-blur-sm md:bg-inherit fixed top-0 z-30 py-2.5 bg-inherit">
             <div className="lg:container lg:mx-auto">
                 <img src={logoimage} alt="Logo" className="w-12 h-auto md:w-16"/>
             </div>
