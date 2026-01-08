81460617e310103b82082ebf80847d7778cd2693
adriamdevs <murigucharles3@gmail.com>
2026-01-08 13:39:09 +0300

    resolve token auth attempt #1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import AiSect from "../components/home/AI";
 import SoftwareEng from "../components/home/SoftwareDev";
 import SystemsEng from "../components/home/SysEng";
 import PageWrapper from "../components/PageWrapper";
-import Carousel from "../components/home/Carousel";
+//import Carousel from "../components/home/Carousel";
 
 
 export default function Home() {
@@ -10,7 +10,8 @@ export default function Home() {
     <PageWrapper>
       <div className="flex flex-col w-full space-y-10">
             <div className="relative flex items-center justify-center h-24 flex-col bg-inherit">
-                <Carousel/>
+                <span className="text-3xl text-center text-gray-800 leading-snug bg-inherit dark:text-gray-50">Developing Advanced Capabilities for Next-Gen Problems</span>
+                {/* <Carousel/> */}
             </div>
              <hr className="mx-6 font-thin text-gray-700/40 dark:text-gray-500"/>
             {/**AI section */}
