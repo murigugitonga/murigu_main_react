fcf4882b4ddeec55f2af3d1b2c473a080790e84d
adriamdevs <murigucharles3@gmail.com>
2026-01-09 14:13:32 +0300

    subtle styling changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,8 +10,8 @@ export default function About() {
           <AboutSection title="Why this?">
             <p>
               I first got into software engineering because I wanted to create products
-              that would not only solve problems but also demonstrate how powerful
-              technology could be as a tool to solve modern problems.
+              that would not only solve problems but also demonstrate how powerful and effective
+              technology could be as a tool.
             </p>
             <p>
               While studying software engineering, my eyes were opened to the wider
@@ -26,39 +26,32 @@ export default function About() {
               as a guiding philosophy for building reliable modern systems.
             </p>
             <p>
-              Over the last couple of years, I’ve worked with talented individuals
-              across industries, learned to view problems from multiple dimensions,
+              Over the last couple of years, I have met, interacted and worked with talented individuals
+              across various industries, learned to view problems from multiple dimensions
               and developed the ability to formulate solutions for seemingly
-              inextricable challenges.
+              inextricable challenges. The gratification is simply more than I could have asked for.
             </p>
           </AboutSection>
           <hr className="font-thin text-gray-700/40 dark:text-gray-500 mx-6"/>
           <AboutSection title="A spotlight on current trends.">
             <p>
-              AI development has in the recent past, triggered a lot of debates, key among them being the possibility
-              of AI taking over workplaces as it advances. There is justified fear in this. AI is here to re-define our way of life and in its wake, reshaping the global industrial complex.
-              The harsh truth is that the revolution will entirely re-define some traditional roles and quite inevitably, eradicate some. However, a key point lies in the fact that
-              AI is still a tool that requires human oversight, creativity and problem-solving skills to be effectively utilized. I believe there is a place for individuals who will suitably retool themselves to work alongside AI.
+              Advancements in the field of artificial intelligence have in the recent past, triggered a lot of debates, key among them being the possibility
+              of AI taking over workplaces as it advances. Experts have aptly christened this as the <strong>AI revolution</strong>. There is palpable, unspoken fear in this. AI is here to re-define our way of life and in its wake, is heavily reshaping the global industrial complex.
             </p>
-          </AboutSection>
-          <hr className="font-thin text-gray-700/40 dark:text-gray-500 mx-6"/>
-          <AboutSection title="A spotlight on current trends.">
             <p>
-              AI development has in the recent past, triggered a lot of debates, key among them being the possibility
-              of AI taking over workplaces as it advances. There is justified fear in this. AI is here to re-define our way of life and in its wake, reshaping the global industrial complex.
-              The harsh truth is that the revolution will entirely re-define some traditional roles and quite inevitably, eradicate some. However, a key point lies in the fact that
-              AI is still a tool that requires human oversight, creativity and problem-solving skills to be effectively utilized. I believe there is a place for individuals who will suitably retool themselves to work alongside AI.
+              The harsh truth is that the revolution will entirely re-define some traditional roles and quite inevitably, eradicate some. However, an important point lies in the fact that
+              AI is still a tool that requires human oversight, creativity and problem-solving skills to be effectively utilized. I believe there is a place for individuals who will adapt by suitably retooling their skillsets to work alongside AI.
             </p>
           </AboutSection>
           <hr className="font-thin text-gray-700/40 dark:text-gray-500 mx-6"/>
           <AboutSection title="A brief glimpse into the future.">
             <p>
-              The last three years have seen rapid advancements in artificial intelligence, notably in the field of machine learning and deep learning. Ubiquitous Large Language Models such as OpenAI's GPT-3 and Google's Gemini
+              The last three years have seen rapid advancements in artificial intelligence, notably in the field of machine learning and deep learning. Ubiquitous large language models such as OpenAI's GPT-5 and Google's Gemini
               have recently crossed a threshold that surpasses an average human's reasoning and now form the backbone of a fast evolving technological world order. Going by the amount of effort and investment major tech companies are pouring into 
               the effort to further advance AI capabilities, it is clear that we are only at the dawn of this revolution.
             </p>
             <p>
-              That also serves as a sufficient indicator that these models will only get better and more powerful. I like seeing them as precursors to the much-hyped AGI. Some(rightfully) consider them to be early forms of AGI. Most industries
+              That also serves as a sufficient indicator that these models will only get better and more powerful. I thinking of them as precursors to the much-hyped AGI. Some(rightfully) consider them to be early forms of AGI. Most industries
               are at the infancy stages of adopting AI technologies, and I believe that the next decade will witness a radical shift in how
               businesses operate, with AI playing a central role in driving innovation and efficiency.
             </p>
